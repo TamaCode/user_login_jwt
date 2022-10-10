e6617a78a1a6a8bc750dd8f045e5ce1d42da6404
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-10 00:38:31 -0300

    Creo JWT al registrar usuario

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
     let errors = { email: '', password: '' };
@@ -16,6 +17,13 @@ const handleErrors = (err) => {
     return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60; // tres dias - segundos
+const createToken = (id) => {
+    return jwt.sign({ id }, 'testing secret string', {
+        expiresIn: maxAge
+    })
+};
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 };
@@ -29,6 +37,8 @@ module.exports.signup_post = async (req, res) => {
 
     try {
         const user = await User.create({ email, password });
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(201).json(user);
     } catch (err) {
         const errors = handleErrors(err);
