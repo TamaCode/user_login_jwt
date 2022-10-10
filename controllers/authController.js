ecbe25b384955706ee37e319bef3d25466c3aaa4
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-10 01:21:49 -0300

    Sign up view response manipulation

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,7 +39,7 @@ module.exports.signup_post = async (req, res) => {
         const user = await User.create({ email, password });
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-        res.status(201).json(user);
+        res.status(201).json({user});
     } catch (err) {
         const errors = handleErrors(err);
         res.status(400).json({errors});
