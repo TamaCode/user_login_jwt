ba88c629cac7ae9a7f4981cc22c48eecb6907e39
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-10 19:31:22 -0300

    handle errors for login page

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,16 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
+    console.log(err.message, err.code);
     let errors = { email: '', password: '' };
+
+    if (err.message === 'incorrect email') {
+        errors.email = 'that email is not registered';
+    }
+
+    if (err.message === 'incorrect password') {
+        errors.password = 'that password is incorrect';
+    }
 
     if (err.code === 11000) {
         errors.email = 'This email is already registered'
@@ -51,8 +60,11 @@ module.exports.login_post = async (req, res) => {
     
     try {
         const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(200).json({ user: user._id });
     } catch(error) {
-        res.status(400).json({});
+        const errors = handleErrors(error);
+        res.status(400).json({errors});
     }
 };
