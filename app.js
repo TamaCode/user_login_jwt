191cc245291357a1acdfe69f69fa517aee63d4d2
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-11 00:09:13 -0300

    implemento auth middleware

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const {requireAuth} = require('./middleware/authMiddleware');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -24,7 +25,7 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 // cookies
