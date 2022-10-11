6b5e2ce5ab7355158201dbb47e4d056090b970fe
Martin Tamareu <mg.tamareu@gmail.com>
2022-10-11 00:33:33 -0300

    checkUser middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
@@ -18,4 +19,27 @@ const requireAuth = (req, res, next) => {
     }
 };
 
-module.exports =  {requireAuth} 
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, 'testing secret string', async (err, decodedToken) => {
+            if (err) {
+                console.log(err.message);
+                res.locals.user = null; // Paso data del server a las views
+                next();
+            } else {
+                console.log(decodedToken);
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user; // Paso data del server a las views
+                next();
+            }
+        });
+
+    } else {
+        res.locals.user = null; // Paso data del server a las views
+        next();
+    }
+};
+
+module.exports =  {requireAuth, checkUser} 
